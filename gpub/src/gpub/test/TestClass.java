d18f8e1ef31541e4a7df3b6b4f82ddf1041c52b9
miyuki-iba <miyuki_iba@toyobo.jp>
2023-08-25 11:53:27 +0900

    Egitからコミット

--- gpub/src/gpub/test/TestClass.java
+++ gpub/src/gpub/test/TestClass.java
@@ -1,1 +1,23 @@
+/**
+ *
+ */
+package gpub.test;
 
+/**
+ * @author 48474
+ *
+ */
+public class TestClass {
+
+	/**
+	 *
+	 */
+	public TestClass() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+}
